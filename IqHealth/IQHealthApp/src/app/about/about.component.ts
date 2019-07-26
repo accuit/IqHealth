8a093212727f2bf3a6092d9ed228aada7f945836
Neeraj <singh.neer@ymail.com>
2019-07-26 19:54:05 +0530

    ft-Api Angular App moved in Main IQHealth Folder

---
+++ IqHealth/IQHealthApp/src/app/about/about.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor(private readonly http: HttpClient) { }
+  user: User;
+  ngOnInit() {
+  }
+
+  feedbackForm = new FormGroup({
+    name: new FormControl('', [Validators.required,
+    Validators.minLength(4)]),
+    phone: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ])
+  });
+
+  onSubmit() {
+
+    const form: any = this.feedbackForm.value;
+    const username = form.email;
+    const password = form.phone;
+
+    this.http.post('http://localhost:8091/api/user/login', { username, password }
+    )
+      .subscribe((res: any) => {
+        if (res)
+          this.user = res;
+      })
+  }
+
+}
+
+interface User {
+  username: string;
+  password: string;
+  firstName: string;
+  LastName: string;
+  Email: String;
+}
---
+++ IqHealth/IQHealthApp/src/app/services/services.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.css']
+})
+export class ServicesComponent implements OnInit {
+
+  title = 'IQ Health App';
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
