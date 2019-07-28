9c4636b1bda3a69718f33e64c0f7fac7d43569ea
Neeraj <singh.neer@ymail.com>
2019-07-28 17:20:08 +0530

    ft-Sql Added Data Project

---
+++ IqHealth/IqHealth.Data.Persistence/Model/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IqHealth.Persistence.Models
+{
+  
+
+    [Table("tbl_User")]
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int BloodGroup { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public int State { get; set; }
+
+        public int UserStatus { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public Boolean IsDeleted { get; set; }
+
+        //public List<Login> Login { get; set; }
+    }
+}
