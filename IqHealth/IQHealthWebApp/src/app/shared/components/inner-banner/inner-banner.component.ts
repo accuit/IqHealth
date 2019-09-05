4b980afbed01461bba8a856a916b9a981aa248bb
Neeraj <singh.neer@ymail.com>
2019-09-06 00:03:20 +0530

    Local css and Js files and congifured inner Banner

--- IqHealth/IQHealthWebApp/src/app/shared/components/inner-banner/inner-banner.component.ts
+++ IqHealth/IQHealthWebApp/src/app/shared/components/inner-banner/inner-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-inner-banner',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InnerBannerComponent implements OnInit {
 
+  @Input('title') title: string;
+  @Input('url') url: string;
+  @Input('subtitle') subtitle: string;
+  @Input('parent') parent: string;
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
