d3e7a8dd4fe603cf6835c332114bad9b0dae7bb0
Neeraj Singh <accuit@outlook.com>
2020-06-13 19:08:47 +0530

    Initial Design Changes

--- HealthIQ.PresentationLayer.AdminWebApp/src/app/dashboard/dashboard.component.ts
+++ HealthIQ.PresentationLayer.AdminWebApp/src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,8 @@ declare const $: any;
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
