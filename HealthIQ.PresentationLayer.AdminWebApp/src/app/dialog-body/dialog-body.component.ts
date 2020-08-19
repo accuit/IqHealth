8d4c543e349615b3274f47e0ca0349b2bbfe3c77
swetarawat <swetarawat0513@gmail.com>
2020-08-20 00:56:25 +0530

    View detail by clicking icon.

---
+++ HealthIQ.PresentationLayer.AdminWebApp/src/app/dialog-body/dialog-body.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { UserMaster } from '../shared/components/user/user.model';
+
+@Component({
+
+  selector: "dialog-b",
+  templateUrl: './dialog-body.component.html',
+  styleUrls: ['./dialog-body.component.css']
+})
+export class DialogBodyComponent {
+  data: UserMaster;
+  cancelButtonText = "Cancel"
+  constructor(
+
+    @Inject(MAT_DIALOG_DATA) private injecteddata: UserMaster,
+    private dialogRef: MatDialogRef<DialogBodyComponent>) {
+      debugger;
+    if (injecteddata) {
+      this.data = injecteddata
+      
+    }
+    //this.dialogRef.updateSize('300vw','300vw')
+  }
+
+  onConfirmClick(): void {
+    this.dialogRef.close(true);
+  }
+
+}
